2bc3e49600d31680def1324eee6ccf6d27eb767d
Raphael WALLIAN <Raphael.WALLIAN@boursorama.fr>
2023-07-01 13:53:55 +0200

    feat(card-view): use CardElement component

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,7 +1,16 @@
 <template>
-
+  <router-link to="/">
+    <button>Retour</button>
+  </router-link>
+  <CardElement :card="card"/>
 </template>
 
 <script setup lang="ts">
+import {useCard} from "@/logic/useCard";
+import {useRoute} from "vue-router";
+import CardElement from "@/components/CardElement.vue";
 
+const route = useRoute()
+const cardId = parseInt(route.params.id as string)
+const card = useCard().get(cardId)
 </script>
